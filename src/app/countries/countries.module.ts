795b56941145baf048c5a36e54ab7c7410e4f83c
Zethess <vibasilio@alumnos.unex.es>
2023-12-01 14:13:39 +0100

    first commit

---
+++ src/app/countries/countries.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+import { CountriesRoutingModule } from './countries-routing.module';
+import { SearchBoxComponent } from '../shared/components/search-box/search-box.component';
+import { SharedModule } from '../shared/shared.module';
+import { CountryTableComponent } from './components/country-table/country-table.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    ByCapitalPageComponent,
+    ByRegionPageComponent,
+    ByCountryPageComponent,
+    CountryPageComponent,
+    CountryTableComponent,
+  ],
+  imports: [
+    CommonModule,
+    CountriesRoutingModule,
+    SharedModule,
+    RouterModule
+  ],
+  exports:[
+
+  ]
+})
+export class CountriesModule { }
